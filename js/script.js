2cdd291bc09715d0a001c7a7bc8585194c682d05
layersony <sammaingi5@gmail.com>
2021-04-09 16:51:24 +0300

    create logic for birthday day

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 function display(){
-  var birthday = document.getElementById("birthday").value; // get date from form
-  var machDate = new Date(birthday); // changing it a machine date readable
-  var date = machDate.getDate(); // date born
-  var month = machDate.getMonth(); // month born diplayed in index form
-  var year = machDate.getFullYear(); // year born
+  let birthday = document.getElementById("birthday").value; // get date from form
+  let machDate = new Date(birthday); // changing it a machine date readable
+  let dt = machDate.getDate(); // date born
+  let mon = machDate.getMonth(); // month born diplayed in index form
+  let yr = machDate.getFullYear(); // year born
 
+  document.getElementById('b-day').innerHTML = dayWeek(dt, (mon + 1), yr);
 
-  console.log(date);
-  console.log(typeof(date));
-
-  console.log(month);
-  console.log(typeof(month));
-
-  console.log(year);
-  console.log(typeof(year));
+  console.log(dt + " " + mon + " " + yr);
 }
 
+// day of the Week
+function dayWeek(day, month, year){
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday'];
+  
+  //jan and feb considered to be of the previous year
+  if (month < 3){
+    month += 12
+    year -= 1
+  }
+  let dOfWeek = (day + parseInt(((month + 1) * 26) / 10) + year + parseInt(year / 4) + 6 * parseInt(year / 100) + parseInt(year / 400) - 1) % 7;
+  
+  exactDay =  days[dOfWeek];
+
+  return exactDay;
+}
